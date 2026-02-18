af7bf8f66da5a03bc9ccc26cecedbb2fd24d4c47
Gullerod <gustavohmsen@gmail.com>
2026-02-18 08:33:32 +0100

    Update Flash_attack.cs

--- Cyberpunk school/Assets/Scripts/Flash_attack.cs
+++ Cyberpunk school/Assets/Scripts/Flash_attack.cs
@@ -2,17 +2,68 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Flash_attack : MonoBehaviour
+public class EnemyTurnController : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    [Header("Enemy Settings")]
+    public List<Card> hand = new List<Card>();     // Cards in enemy hand
+    public float playDelay = 1f;                   // Delay before enemy plays a card
+
+    public bool IsEnemyTurn { get; private set; }
+
+    private void OnEnable()
     {
-        
+        // turn system (if you wanan change it go ahead)
+        TurnManager.OnPlayerTurnEnded += StartEnemyTurn;
     }
 
-    // Update is called once per frame
-    void Update()
+    private void OnDisable()
     {
+        TurnManager.OnPlayerTurnEnded -= StartEnemyTurn;
+    }
+
+    public void StartEnemyTurn()
+    {
+        if (hand.Count == 0)
+        {
+            EndEnemyTurn();
+            return;
+        }
+
+        IsEnemyTurn = true;
+        StartCoroutine(EnemyTurnRoutine());
+    }
+
+    private IEnumerator EnemyTurnRoutine()
+    {
+        // Small wait to feel more natural
+        yield return new WaitForSeconds(playDelay);
+
+        // it picks a card, rigth now its just a random selection,
+        // but you can replace it with a batter ai later.
+        Card chosenCard = ChooseCard();
+        if (chosenCard != null)
+        {
+            chosenCard.Play();
+            hand.Remove(chosenCard);
+        }
+
+        // We can add more actions here if you want enemies with more cards 
         
+        yield return new WaitForSeconds(0.5f);
+        EndEnemyTurn();
+    }
+
+    private Card ChooseCard()
+    {
+        // pick a random card
+        int index = Random.Range(0, hand.Count);
+        return hand[index];
+    }
+
+    private void EndEnemyTurn()
+    {
+        IsEnemyTurn = false;
+        TurnManager.EndEnemyTurn();
     }
 }
+
